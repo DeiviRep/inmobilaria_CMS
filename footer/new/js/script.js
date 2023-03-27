1cb413a87291c3d95b2c117938b1734a4e69ce2d
DeiviRep <patzivargasdavid@gmail.com>
2023-03-27 08:50:36 -0400

    JS y CSS 27-03-23 cambios y mantenimiento

--- footer/new/js/script.js
+++ footer/new/js/script.js
@@ -1,70 +1,303 @@
-$(document).ready(function() {
-	setTimeout(function() {
-		// Declaramos la capa  mediante una clase para ocultarlo
-        $(".banner_gallery").fadeIn(1500);
-		// Transcurridos 5 segundos aparecera la capa midiv2
-    },0);
-});
+//ACTUANDO AL ANCHEAR LA PAGINA
+// window.addEventListener("resize", function () {
+// 	console.log("CONDICION");
+// 	if (window.innerWidth <= 767) {
+// 	  console.log("ESTA RESPONSIVE: " + window.innerWidth);
+// 	  $(document).ready(function() {
+// 		setTimeout(function() {
+// 			// Declaramos la capa  mediante una clase para ocultarlo
+// 			$(".banner_gallery").fadeIn(1500);
+// 			// Transcurridos 5 segundos aparecera la capa midiv2
+// 		},0);
+// 	});
 
-$(document).ready(function() {
-	setTimeout(function() {
-		// Declaramos la capa  mediante una clase para ocultarlo
-        $(".texta").fadeIn(1500);
-		// Transcurridos 5 segundos aparecera la capa midiv2
-    },1000);
-});
+// 	$(document).ready(function() {
+// 		setTimeout(function() {
+// 			// Declaramos la capa  mediante una clase para ocultarlo
+// 			$(".texta5").fadeIn(1500);
+// 			// Transcurridos 5 segundos aparecera la capa midiv2
+// 		},312);
+// 	});
 
-$(document).ready(function() {
-	setTimeout(function() {
-		// Declaramos la capa  mediante una clase para ocultarlo
-        $(".texta2").fadeIn(1500);
-		// Transcurridos 5 segundos aparecera la capa midiv2
-    },2000);
-});
+// 	$(document).ready(function() {
+// 		setTimeout(function() {
+// 			// Declaramos la capa  mediante una clase para ocultarlo
+// 			$(".texta6").fadeIn(1500);
+// 			// Transcurridos 5 segundos aparecera la capa midiv2
+// 		},624);
+// 	});
 
-$(document).ready(function() {
-	setTimeout(function() {
-		// Declaramos la capa  mediante una clase para ocultarlo
-        $(".texta3").fadeIn(1500);
-		// Transcurridos 5 segundos aparecera la capa midiv2
-    },3000);
-});
+// 	$(document).ready(function() {
+// 		setTimeout(function() {
+// 			// Declaramos la capa  mediante una clase para ocultarlo
+// 			$(".texta7").fadeIn(1500);
+// 			// Transcurridos 5 segundos aparecera la capa midiv2
+// 		},936);
+// 	});
 
-$(document).ready(function() {
-	setTimeout(function() {
-		// Declaramos la capa  mediante una clase para ocultarlo
-        $(".texta4").fadeIn(1500);
-		// Transcurridos 5 segundos aparecera la capa midiv2
-    },4000);
-});
+// 	$(document).ready(function() {
+// 		setTimeout(function() {
+// 			// Declaramos la capa  mediante una clase para ocultarlo
+// 			$(".texta8").fadeIn(1500);
+// 			// Transcurridos 5 segundos aparecera la capa midiv2
+// 		},1248);
+// 	});
 
-$(document).ready(function() {
-	setTimeout(function() {
-		// Declaramos la capa  mediante una clase para ocultarlo
+// 	$(document).ready(function() {
+// 		setTimeout(function() {
+// 			// Declaramos la capa  mediante una clase para ocultarlo
+// 			$(".texta").fadeIn(1500);
+// 			// Transcurridos 5 segundos aparecera la capa midiv2
+// 		},1560);
+// 	});
+
+// 	$(document).ready(function() {
+// 		setTimeout(function() {
+// 			// Declaramos la capa  mediante una clase para ocultarlo
+// 			$(".texta2").fadeIn(1500);
+// 			// Transcurridos 5 segundos aparecera la capa midiv2
+// 		},1872);
+// 	});
+
+// 	$(document).ready(function() {
+// 		setTimeout(function() {
+// 			// Declaramos la capa  mediante una clase para ocultarlo
+// 			$(".texta3").fadeIn(1500);
+// 			// Transcurridos 5 segundos aparecera la capa midiv2
+// 		},2184);
+// 	});
+// 	$(document).ready(function() {
+// 		setTimeout(function() {
+// 			// Declaramos la capa  mediante una clase para ocultarlo
+// 			$(".texta4").fadeIn(1500);
+// 			// Transcurridos 5 segundos aparecera la capa midiv2
+// 		},2496);
+// 	});
+// 	} else {
+// 	  console.log("ESTA ANCHO: " + window.innerWidth);
+// 	  $(document).ready(function() {
+// 		setTimeout(function() {
+// 			// Declaramos la capa  mediante una clase para ocultarlo
+// 			$(".banner_gallery").fadeIn(1500);
+// 			// Transcurridos 5 segundos aparecera la capa midiv2
+// 		},0);
+// 	});
+
+// 	$(document).ready(function() {
+// 		setTimeout(function() {
+// 			// Declaramos la capa  mediante una clase para ocultarlo
+// 			$(".texta").fadeIn(1500);
+// 			// Transcurridos 5 segundos aparecera la capa midiv2
+// 		},312);
+// 	});
+
+// 	$(document).ready(function() {
+// 		setTimeout(function() {
+// 			// Declaramos la capa  mediante una clase para ocultarlo
+// 			$(".texta2").fadeIn(1500);
+// 			// Transcurridos 5 segundos aparecera la capa midiv2
+// 		},624);
+// 	});
+
+// 	$(document).ready(function() {
+// 		setTimeout(function() {
+// 			// Declaramos la capa  mediante una clase para ocultarlo
+// 			$(".texta3").fadeIn(1500);
+// 			// Transcurridos 5 segundos aparecera la capa midiv2
+// 		},936);
+// 	});
+
+// 	$(document).ready(function() {
+// 		setTimeout(function() {
+// 			// Declaramos la capa  mediante una clase para ocultarlo
+// 			$(".texta4").fadeIn(1500);
+// 			// Transcurridos 5 segundos aparecera la capa midiv2
+// 		},1248);
+// 	});
+
+// 	$(document).ready(function() {
+// 		setTimeout(function() {
+// 			// Declaramos la capa  mediante una clase para ocultarlo
+// 			$(".texta5").fadeIn(1500);
+// 			// Transcurridos 5 segundos aparecera la capa midiv2
+// 		},1560);
+// 	});
+
+// 	$(document).ready(function() {
+// 		setTimeout(function() {
+// 			// Declaramos la capa  mediante una clase para ocultarlo
+// 			$(".texta6").fadeIn(1500);
+// 			// Transcurridos 5 segundos aparecera la capa midiv2
+// 		},1872);
+// 	});
+
+// 	$(document).ready(function() {
+// 		setTimeout(function() {
+// 			// Declaramos la capa  mediante una clase para ocultarlo
+// 			$(".texta7").fadeIn(1500);
+// 			// Transcurridos 5 segundos aparecera la capa midiv2
+// 		},2184);
+// 	});
+// 	$(document).ready(function() {
+// 		setTimeout(function() {
+// 			// Declaramos la capa  mediante una clase para ocultarlo
+// 			$(".texta8").fadeIn(1500);
+// 			// Transcurridos 5 segundos aparecera la capa midiv2
+// 		},2496);
+// 	});
+// 	}
+//   });
+
+//ACTUANDO AL CARGAR LA PAGINA
+window.addEventListener("load", function () {
+  console.log("2CONDICION");
+  if (window.innerWidth <= 767) {
+    console.log("ESTA RESPONSIVE: " + window.innerWidth);
+    const bloque = document.querySelector(".banner_gallery");
+    bloque.style.display = `flex`
+    //   $(document).ready(function() {
+    // 	setTimeout(function() {
+    // 		// Declaramos la capa  mediante una clase para ocultarlo
+    // 		$(".banner_gallery").fadeIn(1500);
+    // 		// Transcurridos 5 segundos aparecera la capa midiv2
+    // 	},0);
+    // });
+
+    $(document).ready(function () {
+      setTimeout(function () {
+        // Declaramos la capa  mediante una clase para ocultarlo
         $(".texta5").fadeIn(1500);
-		// Transcurridos 5 segundos aparecera la capa midiv2
-    },5000);
-});
+        // Transcurridos 5 segundos aparecera la capa midiv2
+      }, 312);
+    });
 
-$(document).ready(function() {
-	setTimeout(function() {
-		// Declaramos la capa  mediante una clase para ocultarlo
+    $(document).ready(function () {
+      setTimeout(function () {
+        // Declaramos la capa  mediante una clase para ocultarlo
         $(".texta6").fadeIn(1500);
-		// Transcurridos 5 segundos aparecera la capa midiv2
-    },6000);
-});
+        // Transcurridos 5 segundos aparecera la capa midiv2
+      }, 624);
+    });
 
-$(document).ready(function() {
-	setTimeout(function() {
-		// Declaramos la capa  mediante una clase para ocultarlo
+    $(document).ready(function () {
+      setTimeout(function () {
+        // Declaramos la capa  mediante una clase para ocultarlo
         $(".texta7").fadeIn(1500);
-		// Transcurridos 5 segundos aparecera la capa midiv2
-    },7000);
-});
-$(document).ready(function() {
-	setTimeout(function() {
-		// Declaramos la capa  mediante una clase para ocultarlo
+        // Transcurridos 5 segundos aparecera la capa midiv2
+      }, 936);
+    });
+
+    $(document).ready(function () {
+      setTimeout(function () {
+        // Declaramos la capa  mediante una clase para ocultarlo
         $(".texta8").fadeIn(1500);
-		// Transcurridos 5 segundos aparecera la capa midiv2
-    },8000);
+        // Transcurridos 5 segundos aparecera la capa midiv2
+      }, 1248);
+    });
+
+    $(document).ready(function () {
+      setTimeout(function () {
+        // Declaramos la capa  mediante una clase para ocultarlo
+        $(".texta").fadeIn(1500);
+        // Transcurridos 5 segundos aparecera la capa midiv2
+      }, 1560);
+    });
+
+    $(document).ready(function () {
+      setTimeout(function () {
+        // Declaramos la capa  mediante una clase para ocultarlo
+        $(".texta2").fadeIn(1500);
+        // Transcurridos 5 segundos aparecera la capa midiv2
+      }, 1872);
+    });
+
+    $(document).ready(function () {
+      setTimeout(function () {
+        // Declaramos la capa  mediante una clase para ocultarlo
+        $(".texta3").fadeIn(1500);
+        // Transcurridos 5 segundos aparecera la capa midiv2
+      }, 2184);
+    });
+    $(document).ready(function () {
+      setTimeout(function () {
+        // Declaramos la capa  mediante una clase para ocultarlo
+        $(".texta4").fadeIn(1500);
+        // Transcurridos 5 segundos aparecera la capa midiv2
+      }, 2496);
+    });
+  } else {
+    console.log("ESTA ANCHO: " + window.innerWidth);
+	const bloque = document.querySelector(".banner_gallery");
+    bloque.style.display = `flex`
+    // $(document).ready(function () {
+    //   setTimeout(function () {
+    //     // Declaramos la capa  mediante una clase para ocultarlo
+    //     $(".banner_gallery").fadeIn(1500);
+    //     // Transcurridos 5 segundos aparecera la capa midiv2
+    //   }, 0);
+    // });
+
+    $(document).ready(function () {
+      setTimeout(function () {
+        // Declaramos la capa  mediante una clase para ocultarlo
+        $(".texta").fadeIn(1500);
+        // Transcurridos 5 segundos aparecera la capa midiv2
+      }, 312);
+    });
+
+    $(document).ready(function () {
+      setTimeout(function () {
+        // Declaramos la capa  mediante una clase para ocultarlo
+        $(".texta2").fadeIn(1500);
+        // Transcurridos 5 segundos aparecera la capa midiv2
+      }, 624);
+    });
+
+    $(document).ready(function () {
+      setTimeout(function () {
+        // Declaramos la capa  mediante una clase para ocultarlo
+        $(".texta3").fadeIn(1500);
+        // Transcurridos 5 segundos aparecera la capa midiv2
+      }, 936);
+    });
+
+    $(document).ready(function () {
+      setTimeout(function () {
+        // Declaramos la capa  mediante una clase para ocultarlo
+        $(".texta4").fadeIn(1500);
+        // Transcurridos 5 segundos aparecera la capa midiv2
+      }, 1248);
+    });
+
+    $(document).ready(function () {
+      setTimeout(function () {
+        // Declaramos la capa  mediante una clase para ocultarlo
+        $(".texta5").fadeIn(1500);
+        // Transcurridos 5 segundos aparecera la capa midiv2
+      }, 1560);
+    });
+
+    $(document).ready(function () {
+      setTimeout(function () {
+        // Declaramos la capa  mediante una clase para ocultarlo
+        $(".texta6").fadeIn(1500);
+        // Transcurridos 5 segundos aparecera la capa midiv2
+      }, 1872);
+    });
+
+    $(document).ready(function () {
+      setTimeout(function () {
+        // Declaramos la capa  mediante una clase para ocultarlo
+        $(".texta7").fadeIn(1500);
+        // Transcurridos 5 segundos aparecera la capa midiv2
+      }, 2184);
+    });
+    $(document).ready(function () {
+      setTimeout(function () {
+        // Declaramos la capa  mediante una clase para ocultarlo
+        $(".texta8").fadeIn(1500);
+        // Transcurridos 5 segundos aparecera la capa midiv2
+      }, 2496);
+    });
+  }
 });
